f503e1c2d865063a7d3e8336e993d70eaf2b88a4
zhouf <41719122@qq.com>
2020-07-09 09:20:44 +0800

    详情页更新

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,11 +1,22 @@
-<!--  -->
+<!-- 详情页面 -->
 <template>
-  
+  <div>
+    <nav-bar>
+      <div slot="left" @click="backHome">
+         <i class="el-icon-arrow-left yellow"></i>
+      </div>
+    </nav-bar>
+    <h2>详情</h2>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
   export default {
-    name: 'Detail'
+    name: 'Detail',
+    components: {
+      NavBar
+    }
     
   }
 </script>
