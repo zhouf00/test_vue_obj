9cf6bb60b00d4b1c1332315b99f4c3b3a40e2879
zhouf00 <49618748+zhouf00@users.noreply.github.com>
2020-07-15 17:49:01 +0800

    测试

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -26,7 +26,7 @@
     },
     components:{
       get_token() {
-        this.$router.
+        
       }
     }
   }
--- src/views/test/Test.vue
+++ src/views/test/Test.vue
@@ -14,7 +14,7 @@
 
   export default {
     name: 'Test',
-    components:{
+    components:{ 
       VueWxlogin
     },
     data() {
